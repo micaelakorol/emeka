886079eefedb40b30297dd131d2a06016f43a714
Micaela Korol <micaelakorol@gmail.com>
2024-07-19 12:40:33 -0300

    add new project

--- src/app/utils/dataProject.js
+++ src/app/utils/dataProject.js
@@ -3,25 +3,27 @@ const dataProject = {
     {
       id: 0,
       infoProject: "Sitio web informativo.",
-      description:
-        "Sitio web informativo, se presentan sus servicios, beneficios y cuenta con formularios de contacto.",
       image: "https://i.ibb.co/bd3hnV9/ciclo.png",
       visit: "https://ciclomarketing.netlify.app/",
     },
     {
       id: 4,
       infoProject: "Sitio web para Herrería Javier",
-      description:
-        "Exhibe los servicios que ofrece, su ubicación, redes sociales y un pequeño portafolio con sus trabajos.",
       image:
         "https://res.cloudinary.com/dgqyqd2pa/image/upload/v1708346281/landing_page_herreria/herreria_b8tsco.png",
       visit: "https://herreriajavier.website/",
+    },
+    {
+      id: 114,
+      infoProject: "Maquetación formulario de registro",
+      image:
+        "https://res.cloudinary.com/dgqyqd2pa/image/upload/v1721403531/emeka.site/maquetacion_udjd6r.png",
+      visit: "https://maquetacion-formulario.netlify.app/",
     },
 
     {
       id: 41,
       infoProject: "Landing Page para Ebook",
-      description: "Landing Page para venta de Ebook.",
       image:
         "https://res.cloudinary.com/dgqyqd2pa/image/upload/v1711480017/emeka.site/ciclo_e4hxrx.png",
       visit: "https://htmlfundamental-byciclo.netlify.app/",
